b99fa1956ba1172143fed622601c722074cc9bf8
mohamedHaney <mohaney745@gmail.com>
2024-11-05 01:20:23 +0200

    #

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -8,7 +8,7 @@ export default function PostCard({ post }) {
   return (
     <Link
       to={`/post/${post.slug}`}
-      className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[270px] transition-all'
+      className='group relative w-full border border-teal-500 hover:border-2 h-[350px] overflow-hidden rounded-lg sm:w-[270px] transition-all'
     >
       <div>
         {/* Render video if available; otherwise, render image */}
@@ -22,20 +22,19 @@ export default function PostCard({ post }) {
               e.target.currentTime = 0;
             }}
             alt='post cover'
-            className='h-[200px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20'
+            className='h-[230px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20'
             // controls // Add controls if desired
           />
         ) : (
           <img
             src={post.image}
             alt='post cover'
-            className='h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20'
+            className='h-[230px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20'
           />
         )}
       </div>
       <div className='p-3 flex flex-col gap-2'>
         <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
-        <p className='text-lg font-semibold line-clamp-2'>{post.content}</p>
         <span className='italic text-sm'>{post.category}</span>
         <Link
           to={`/post/${post.slug}`}
