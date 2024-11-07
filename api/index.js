e0f258a16c4adec99e20ff35cef8e0a20dfff2cc
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2024-11-07 04:17:48 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,17 +25,10 @@ const __dirname = path.resolve();
 const app = express();
 
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Enable sending cookies with the request
+  origin: 'http://localhost:5173', // Allow only your frontend's origin
+  credentials: true, // Include cookies if needed
 }));
+
 
 
 app.use(express.json());
