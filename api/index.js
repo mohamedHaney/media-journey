24713a71689836a7cffdfb9a5892bd8902996b7e
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2024-11-08 21:10:48 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const __dirname = path.resolve();
 const app = express();
 
 app.use(cors({
-  origin: 'https://media-journey.vercel.app', // Allow only your frontend's origin
+  origin: 'https://media-journey-6pl4.vercel.app', // Allow only your frontend's origin
   credentials: true, // Include cookies if needed
 }));
 
